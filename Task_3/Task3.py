ff4dbde832cc31c3f73d7440982f80ea035417d4
Maxim Denissov <DM7894289@gmail.com>
2022-12-23 18:13:42 +0600

    Add Task 3

---
+++ Task_3/Task3.py
@@ -0,0 +1,22 @@
+# 3 Задайте последовательность чисел. 
+# Напишите программу, которая выведет список 
+# неповторяющихся элементов исходной последовательности.
+
+import random
+
+def num_list_generator(list_length, min_value, max_value):
+    result_list =[]
+    for i in range (list_length):
+        result_list.append(random.randint(min_value,max_value))
+    return result_list
+
+def num_list_uniq (list):
+    result_list = []
+    for i in list:
+        if list.count(i) == 1:
+            result_list.append(i)
+    return(result_list)
+
+my_list = num_list_generator(14,0,10)
+print(my_list)
+print(num_list_uniq(my_list))
